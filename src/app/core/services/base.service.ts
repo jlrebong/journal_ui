7ec93f482682e97c60e044aead38fa669c14c1bb
unknown <jrebong0@gmail.com>
2023-12-11 02:02:41 +0800

    add custom webpack

--- src/app/core/services/base.service.ts
+++ src/app/core/services/base.service.ts
@@ -10,7 +10,8 @@ export class BaseService {
   protected url;
 
   constructor(protected http: HttpClient) { 
-    this.url = 'http://127.0.0.1:5000/api';
+    let url = process.env["SERVICE_API"] || 'http://127.0.0.1:5000/api'; 
+    this.url = url;
   }
 
   protected getHeader() {
